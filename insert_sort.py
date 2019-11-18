87352ed81e7aec9ef671e217b25f2d5afd50a823
Demyan <47394046+Injector1@users.noreply.github.com>
2019-11-18 17:29:12 +0500

    Update insert_sort.py

---
+++ insert_sort.py
@@ -0,0 +1,13 @@
+def sort(array):
+	for i in range(len(array)):
+		j = i - 1
+		element = array[i]
+		while array[j] > element and j >= 0:
+			array[j + 1] = array[j]
+			j -= 1
+		array[j + 1] = element
+	return array
+
+
+array = [5, 2, 3, 1, 6, 9, 3, 8, 10]
+print(sort(array))
